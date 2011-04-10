198209689f5d2b1b32b6a5fc9221fd720d000f05
Sonal Gupta <sonal@constantine.cms.caltech.edu>
2011-04-10 14:17:10 -0700

    Crawler

---
+++ crawler.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+"""
+A webcrawler that crawls webpages in the *.caltech.edu domain
+
+TODO:
+* Clean up (e.g. make class + fix style (line width etc.))
+* Implement robots.txt adherence
+* Make it a class rather than a script
+* Expand w/ possibility of watching in-degree (extra credit)
+
+BUGS (minor):
+* for some reason one entry of the form '~kip/"' made it into the results.
+* for some reason the result for 'http://www.its.caltech.edu/' has got no value
+
+"""
+from fetcher import fetch_links
+import re, csv
+from collections import deque
+import random
+
+def crawler(URL_seeds=["http://www.caltech.edu"]):
+	#URL_seeds = ["http://www.caltech.edu"]
+
+	MAX_ITERATIONS = 200000 # How many iterations? Finite execution, despite tons of garbage pages.
+	MAX_RESULTS = 100 #20010 # How many results? Finite execution, rather than crawling entire reachable(URL_seeds)'
+	URLS_FETCH = 3
+	
+	DOMAIN_PATTERN = '^http[s]?://([^.]+?[.])*?.?' # restrict search to this domain
+	def search(x): return re.search(DOMAIN_PATTERN, x)
+	def match(x): return re.match(DOMAIN_PATTERN, x)
+	
+	URLs_found = set(URL_seeds) # set holding pages all URLs encountered
+	result = {} # dictionary holding URL + links out	
+	# Using both deque (for breadth-first + efficiency) and a set (for ease of eliminating duplicates)
+	queue = deque(URL_seeds) # queue holding pages to process
+	queue_set = set(URL_seeds) # set holding pages in queue
+	
+	count = 0
+	output = 1
+	while (queue) and (count < MAX_ITERATIONS) and (len(result) < MAX_RESULTS):
+		# Process a page
+		count += 1
+		#URL = queue.pop() # next page is arbitrary set element
+		URL = queue.popleft() # fetch next page (FIFO -> Breath First)
+		links_on_page = fetch_links(URL)
+		
+		if (links_on_page is not None):
+			# Store results
+			links_on_page = filter(search, links_on_page) # don't examine pages not matching DOMAIN_PATTERN
+			links = set(links_on_page) # (set) links encountered on page
+			result[URL] = len(links) # (dictionary) add page and its number of links
+			
+			# Add unencountered pages to queue
+			#links.difference_update(URLs_found) # throw away all URLs previously encountered
+			#queue_set.update(links) # add previously unencountered pages to queue
+			new_pages = links.difference(queue_set, URLs_found) # throw away all URLs previously encountered
+			queue_set.update(new_pages) # add pages to set
+			queue.extend(new_pages) # add pages to queue
+			
+		# Print progress
+		if (((count % output) == 0) or ((len(result) % 100) == 0)):
+			output = count / output
+			print "Progress: %d pages crawled. %d results found. Queue of %d" % (count, len(result), len(queue))
+			
+	
+	# Output results
+	print "\nFinished!"
+	print "Found %d results, by crawling through %d pages. Queue at termination: %d" % (len(result), count, len(queue))
+
+	# TODO: Output queue
+
+	# Write results to CSV file
+	output_file = "output.txt"
+	f = open(output_file, 'a')
+	URL_List = []
+	try:
+	    for key in result:
+		URL_List.append(key)
+	    	f.write(key + '\n')                            
+	finally:
+	    f.close()
+	print "\nWrote results to file %s" % output_file	
+
+	result_file = "result.txt"
+	f = open(result_file, 'a')
+	try:
+	    for i in range(URLS_FETCH):
+		if URL_List != []:
+		    url = random.choice(URL_List)
+		    f.write(url + '\n')
+	finally:
+	    f.close()
+	print "\nWrote final results to file %s" %result_file       
+
+#Entrance of this script, just like the "main()" function in C.
+
+if __name__ == "__main__":
+    import sys, pstats, cProfile, os.path
+
+    if len(sys.argv)==1 or not sys.argv[1].startswith("http://"):
+       if len(sys.argv)==1:
+    		# No args: Use default startpage
+    		print crawler()
+       else:
+              try:
+                   i = 0;
+                   input_filename = sys.argv[1];
+                   f1 = open(input_filename, "r")
+                   for line in f1:
+                         list = []
+                         list.append(line)
+                         i += 1
+                         print "Number %d" %i
+                         print list
+                         print crawler(list)                   
+                   f1.close()
+
+              except IOError:
+                   print >> sys.stderr, "Input file doesn't exist"
+    else:
+    	# One or more webpages specified. Seed crawl with these.
+        print sys.argv[1:]
+        print crawler(sys.argv[1:])
+
+#p = pstats.Stats('crawlprof')
+#p.sort_stats('time').print_stats(10)
+
---
+++ fetcher.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+"""
+A simple program to extract links in a web page, by Minghong Lin, for CS/EE144.
+If you use Python, you can import this file and simply call the function
+"fetch_links(URL)" to get a list (may be empty) which contains all hyperlinks
+in the web page specified by the URL. It returns None if the URL is not a valid
+HTML page or some error occurred.
+If you use other programming language, you can make a system call to execute this
+file and pipe the output to your program. The command is
+"python fetcher.py http://..."
+The output is a list (may be empty "[]") of hyperlinks which looks like:
+"['http://today.caltech.edu', 'http://www.caltech.edu/copyright/']"
+The output is "None" if the URL is not a valid HTML page or some error occurred.
+"""
+#A simple HTML parser named SGMLParser is in the standard Python module sgmllib.
+#urljoin is used to combine a base URL and a relative URL to an absolute URL.
+#We would use urllib2 to retrieve the webpage.
+from sgmllib import SGMLParser
+from urlparse import urljoin
+import urllib2
+
+#SGMLParser is like a template for implementing a HTML parser. All its methods
+#for handling HTML tags are "do-nothing" methods. We have to inherit from it
+#and override some methods in order to extract the hyperlinks.
+class URLLister(SGMLParser):
+    
+    #Reset the parser by calling this method.
+    def reset(self):
+        SGMLParser.reset(self)
+        self.urls = []
+
+    #Just to simplify the methods start_a/do_area/start_frame/start_iframe.
+    def _extract_url(self,attrs,keyword):
+        href = [v for (k, v) in attrs if k==keyword]
+        if href:
+            self.urls.extend(href)
+
+    #start_TAG() and do_TAG() are called by SGMLParser whenever it finds a TAG.
+    #"<a> ... </a>" tag may contain an "href" attribute which is a hyperlink.
+    def start_a(self, attrs):
+        self._extract_url(attrs,'href')
+
+    #"<area ...>" tag may contain an "href" attribute which is a hyperlink.            
+    def do_area(self, attrs):
+        self._extract_url(attrs,'href')
+
+    #"<frame ...>" tag may contain an "src" attribute which is a hyperlink.            
+    def do_frame(self, attrs):
+        self._extract_url(attrs,'src')
+
+    #"<iframe ...>" tag may contain an "src" attribute which is a hyperlink.            
+    def do_iframe(self, attrs):
+        self._extract_url(attrs,'src')
+
+    #Refine the hyperlinks: ignore the parameters in dynamic URL, converting
+    #all hyperlinks to absolute URLs, remove duplicated URLs.
+    def get_links(self,current_url):
+        res=set()       #Use datastructure "set" to avoid duplicated URLs.
+        for url in self.urls:
+            url=url.split('?')[0]   #Extract the part before '?' if any.
+            url=url.split('#')[0]   #Extract the part before '#' if any.
+            url=urljoin(current_url,url.strip())   #Convert to absolute URL.
+            if url.startswith("http://") or url.startswith("https://"):
+                res.add(url)
+        res.discard(current_url)   #Self-link is removed
+        return list(res)
+
+#Fetch an HTML file, return the real (redirected) URL and the content.
+def fetch_html_page(url):
+    content=None
+    real_url=url
+    try:
+        urllib2.socket.setdefaulttimeout(10)  #Bad page, timeout in 10s.
+        usock = urllib2.urlopen(url)
+        real_url=usock.url  #real_url will be changed if there is a redirection. 
+        if "text/html" in usock.info()['content-type']:
+            content=usock.read()    #Fetch it if it is html but not mp3/avi/...
+        usock.close()
+    except:
+        pass
+    return (real_url, content)
+
+#Fetch the hyperlinks. First fetch the content then use URLLister to parse them.
+def fetch_links(url):
+    links=None
+    try:
+        parser = URLLister()
+        ru,s=fetch_html_page(url)
+        if s is not None:
+            parser.feed(s)
+            parser.close()
+            links=parser.get_links(ru)
+    except:
+        pass
+    return links
+
+#Entrance of this script, just like the "main()" function in C.
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv)!=2 or not sys.argv[1].startswith("http://"):
+        pn=sys.argv[0]
+        i=max(pn.rfind("\\"),pn.rfind("/"))+1
+        print "usage: %s http://...\tShow all hyperlinks in an HTML page."%pn[i:]
+    else:
+        print fetch_links(sys.argv[1])
